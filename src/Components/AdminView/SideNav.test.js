58410766dddf7588366d9f6f6ac64fce8f80dcae
Li Swafford <liswafford@yahoo.com>
2019-02-26 00:12:57 -0600

    adding test

--- src/Components/AdminView/SideNav.test.js
+++ src/Components/AdminView/SideNav.test.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { shallow } from "enzyme";
+import SideNav from "./SideNav";
 
 describe("SideNav", () => {
-  it("should ");
+  let component;
+
+  it("should toggle the state based on e.target.id ", () => {
+    component = shallow(<SideNav />);
+    component.find("#toggler1").simulate("click");
+    expect(component.state().toggler1).toBe(true);
+  });
 });
